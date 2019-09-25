1532f1edb2046c9a6a322e049246e1502b27a6f6
“Marko <marko.leht@khk.ee>
2019-09-25 11:29:47 +0300

    Muutujad - HTML vormi sisestamine ja kasutamine

---
+++ 25.09.2019/input_do.php
@@ -0,0 +1,7 @@
+<?php
+// muutuja defineerimine ja v22rtuse v6tmine vormist
+$nimi = $_GET['nimi'];
+// v2ljastamine
+echo 'Nimi: '.$nimi.'<br>';
+echo '<hr>';
+echo '<a href="input.php">Sisesta andmed uuesti</a>';
