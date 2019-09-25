bf1fd2ad1ca655bdfbd0c531913ff2741626e9c3
“Marko <marko.leht@khk.ee>
2019-09-25 10:43:20 +0300

    Minu info

--- 25.09.2019/ex_1.php
+++ 25.09.2019/ex_1.php
@@ -6,4 +6,12 @@
  * vanus: 35
  * pikkus: 1.85
  * kaal(kg): 78
-* */
+ */
+ echo'<h1>INfo minust</h1>';
+ echo'Eesnimi: Marko <br>';
+ echo'Perekonnanimi: Leht<br>';
+ echo'e-mail: marko.leht@khk.ee<br>';
+ echo'vanus: 35<br>';
+ echo'pikkus(m): 1.85<br>';
+ echo'kaal(kg): 78<br>';
+
