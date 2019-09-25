2e2eff1da8d82d22c1195d6f4d406dce38ced2b6
“Marko <marko.leht@khk.ee>
2019-09-25 11:43:53 +0300

    25.09.2019-README.md

--- 25.09.2019/input.php
+++ 25.09.2019/input.php
@@ -3,5 +3,21 @@
         <label for="nimi">Nimi:</label>
         <input type="text" id="nimi" name="nimi">
     </div>
+
+        <div>
+            <label for="pikkus">Pikkus:</label>
+            <input type="text" id="pikkus" name="">
+        </div>
+
+    <div>
+        <label for="kaal">Kaal:</label>
+        <input type="text" id="kaal" name="">
+    </div>
+
+    <div>
+        <label for="KMI"></label>
+        <input type="text" id="" name="">
+    </div>
+
     <input type="submit" value="saada">
 </form>
