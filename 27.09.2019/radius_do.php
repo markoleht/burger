9ba95f17de02601d23f237a3113fba4e51a7018b
“Marko <marko.leht@khk.ee>
2019-09-27 09:51:57 +0300

    27.09.2019 pi v22rtuse  arvutamine ja ymmardamisfunktisooni kasutamine

--- 27.09.2019/radius_do.php
+++ 27.09.2019/radius_do.php
@@ -6,12 +6,16 @@ $raadius = $_GET['raadius'];
 echo 'Sisestatud raadius on ' .$raadius.'<br>';
 //arvutused
 //ringi pindala
-$ringiPindala = 3.14 * $raadius * $raadius;
+$ringiPindala = pi() * $raadius * $raadius;
 // ringi ymberm66t
-$ringiUmbermoot = 3.14 * 2 * $raadius;
+$ringiUmbermoot = pi() * 2 * $raadius;
 // muutujate andmete tyypi ja v22rtuste kontroll
 //var_dump($ringiPindala);
 //var_dump($ringiUmbermoot);
 //v2ljastame tulemused
-echo 'Ringi pindala on ' .$ringiPindala.'<br>';
-echo 'Ringi ymberm66t on ' .$ringiUmbermoot.'<br>';
+echo '<hr>';
+$ringiPindalaUmmardatud = round($ringiPindala, 3);
+echo 'Ringi pindala on ' .$ringiPindalaUmmardatud.'<br>';
+echo 'Ringi ymberm66t on ' .round($ringiUmbermoot, 3).'<br>';
+//cmnd+(hiirega liigud k2su peale ja voila
+//php.net
