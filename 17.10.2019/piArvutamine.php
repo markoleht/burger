b277bda7bb8d151e8dd4b1aebb94828155f44c3b
markoleht <54833627+markoleht@users.noreply.github.com>
2019-10-24 13:33:52 +0300

    pi arvutamine while abil

--- 17.10.2019/piArvutamine.php
+++ 17.10.2019/piArvutamine.php
@@ -1,24 +1,22 @@
 <?php
 //pi value
-//define max count of elements
-$maxCount = 1000000;
-//define count for loops
+// define max count of elements
+$maxCount = 100000000;
+// define count for loop
 $count = 1;
-//define sum of elements
-
+// define sum of elements
 $sum = 0;
-while ($count <= $maxCount){
-    $element =1 / (2 * $count - 1);
-    if($count % 2 !=0){
-        $sum = $sum + $element;
-    } else{
-        $sum = $sum - $element;
-    }
-    $count++;
+while ($count <= $maxCount) {
+  $element = 1 / (2 * $count - 1);
+  if($count % 2 != 0) {
+    $sum = $sum + $element;
+  } else {
+    $sum = $sum - $element;
+  }
+  $count++;
 }
-echo 'arvutatud pi =' .($sum*4).'<br>';
-echo 'kontroll pi = '.pi();
-
+echo 'Arvutatud pi = '.($sum * 4).'<br>';
+echo 'Kontroll pi  = '.pi();
 
 
 
