54044018612761549f7a922d6fc1dd73af03db38
markoleht <54833627+markoleht@users.noreply.github.com>
2019-10-24 17:36:25 +0300

    Update temperature.php

--- 07.10.2019/temperature.php
+++ 07.10.2019/temperature.php
@@ -1,9 +1,7 @@
 <?php
-/**
- * file name: temperature.php;
- * autor: anna.karutina;
- * date: 07.10.2019;
- */
+
+//NB: Kopeeritud õpetaja repost kuna puudusin koolist.
+
 // define variables
 $celsius = 5;
 $fahrenheit = (9 / 5) * $celsius + 32;
