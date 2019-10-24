f260e6b2101a04f1c4d4bbd89d50a4eb81fb3949
markoleht <54833627+markoleht@users.noreply.github.com>
2019-10-24 12:49:08 +0300

    Update input.php

--- 25.09.2019/input.php
+++ 25.09.2019/input.php
@@ -3,21 +3,13 @@
         <label for="nimi">Nimi:</label>
         <input type="text" id="nimi" name="nimi">
     </div>
-
-        <div>
-            <label for="pikkus">Pikkus:</label>
-            <input type="text" id="pikkus" name="">
-        </div>
-
     <div>
-        <label for="kaal">Kaal:</label>
-        <input type="text" id="kaal" name="">
+        <label for="pikkus">Pikkus (m):</label>
+        <input type="number" step="0.01" min="0.0" id="pikkus" name="pikkus">
     </div>
-
     <div>
-        <label for="KMI"></label>
-        <input type="text" id="" name="">
+        <label for="kaal">Kaal (kg):</label>
+        <input type="number" min="0" id="kaal" name="kaal">
     </div>
-
-    <input type="submit" value="saada">
+    <input type="submit" value="Saada">
 </form>
