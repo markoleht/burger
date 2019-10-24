8c9f540a4fd9992d270da8e5822a39e114623c32
markoleht <54833627+markoleht@users.noreply.github.com>
2019-10-24 13:23:46 +0300

    if - else if - else lause

---
+++ 08.10.2019/pos-neg.php
@@ -0,0 +1,25 @@
+<?php
+// define variables
+$number = -25;
+/*var_dump($number < 0);
+echo '<br>';
+// if statement - if number is < than 0
+if($number < 0) {
+    // work if number < than 0
+    echo $number.' on negatiivne<br>';
+}*/
+// control the value of logical statement
+var_dump($number > 0);
+echo '<br>';
+// if statement - if number is > than 0
+if($number > 0) {
+    // work if number > than 0
+    echo $number.' on positiivne<br>';
+} else if($number < 0) {
+    // work if number < than 0
+    echo $number.' on negatiivne<br>';
+} else {
+    echo $number.' on null<br>';
+}
+// the program is continue
+echo 'programmi lõpp';
